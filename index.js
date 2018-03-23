9a9ccddc5d631e1c513d165c4e122f568db7c2da
David Chung <davidc4747@yahoo.com>
2018-03-22 17:08:47 -0700

    Create enemies

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 
 // TODO: canvas should fit viewport
-// This is gonna get reeeeaallly messy...
+// This is gonna get reeeeaallly messy!!!
 
 
 /*====================*\
@@ -10,9 +10,9 @@
 const CANVAS_WIDTH = 800;
 const CANVAS_HEIGHT = 600;
 
-let canvas = document.querySelector(".game-canvas");
-canvas.width = CANVAS_WIDTH;
-canvas.height = CANVAS_HEIGHT;
+let mainCanvas = document.querySelector(".game-canvas");
+mainCanvas.width = CANVAS_WIDTH;
+mainCanvas.height = CANVAS_HEIGHT;
 
 let lightMapCanvas = document.createElement("canvas");
 lightMapCanvas.width = CANVAS_WIDTH;
@@ -22,15 +22,18 @@ let shadowCanvas = document.querySelector(".shadow-canvas");
 shadowCanvas.width = CANVAS_WIDTH;
 shadowCanvas.height = CANVAS_HEIGHT;
 
-let mainContext = canvas.getContext("2d");
+let mainContext = mainCanvas.getContext("2d");
 let lightMap = lightMapCanvas.getContext("2d");
 let shadowContext = shadowCanvas.getContext("2d");
 
 
 
 
+/* Enemy Vars */
+let enemies = [];
+
 /* Player Vars */
-let position = { x: canvas.width / 2, y: canvas.height / 2 };
+let position = { x: mainCanvas.width / 2, y: mainCanvas.height / 2 };
 let speed = 7;
 let stopVal = 0;
 
@@ -53,9 +56,21 @@ setInterval(function () {
 
 function draw() {
     // Clear canvaseses
-    mainContext.clearRect(0, 0, canvas.width, canvas.height);
-    lightMap.clearRect(0, 0, canvas.width, canvas.height);
-    shadowContext.clearRect(0, 0, canvas.width, canvas.height);
+    mainContext.clearRect(0, 0, mainCanvas.width, mainCanvas.height);
+    lightMap.clearRect(0, 0, mainCanvas.width, mainCanvas.height);
+    shadowContext.clearRect(0, 0, mainCanvas.width, mainCanvas.height);
+
+
+
+
+
+    /*====================*\
+        #Draw Enemies
+    \*====================*/
+
+    enemies.forEach(function (enemy) {
+        enemy.draw(mainContext);
+    });
 
 
 
@@ -66,7 +81,7 @@ function draw() {
 
     let playerImage = new Image();
     playerImage.src = "./imgs/space-ship-svgrepo-com.svg"
-    mainContext.drawImage(playerImage, position.x, position.y, 64, 64);
+    mainContext.drawImage(playerImage, position.x, position.y, 50, 50);
 
 
 
@@ -85,29 +100,29 @@ function draw() {
     playerLight.addColorStop(0, "white");
     playerLight.addColorStop(1, "transparent");
     lightMap.fillStyle = playerLight;
-    lightMap.fillRect(0, 0, canvas.width, canvas.height);
+    lightMap.fillRect(0, 0, mainCanvas.width, mainCanvas.height);
 
     // Create gradient
     let light2 = shadowContext.createRadialGradient(150, 300, 30, 150, 300, 100);
     light2.addColorStop(0, "white");
     light2.addColorStop(1, "transparent");
     lightMap.fillStyle = light2;
-    lightMap.fillRect(0, 0, canvas.width, canvas.height);
+    lightMap.fillRect(0, 0, mainCanvas.width, mainCanvas.height);
 
     // Create gradient
     let light3 = shadowContext.createRadialGradient(650, 300, 30, 650, 300, 100);
     light3.addColorStop(0, "white");
     light3.addColorStop(1, "transparent");
     lightMap.fillStyle = light3;
-    lightMap.fillRect(0, 0, canvas.width, canvas.height);
+    lightMap.fillRect(0, 0, mainCanvas.width, mainCanvas.height);
 
     // Create Wave Gradient!!
-    let lightWave = shadowContext.createRadialGradient(position.x, position.y, canvas.width * stopVal / 2, position.x, position.y, canvas.width * stopVal);
+    let lightWave = shadowContext.createRadialGradient(position.x, position.y, mainCanvas.width * stopVal / 2, position.x, position.y, mainCanvas.width * stopVal);
     lightWave.addColorStop(0, "transparent");
     lightWave.addColorStop(stopVal, "white");
     lightWave.addColorStop(1, "transparent");
     lightMap.fillStyle = lightWave;
-    lightMap.fillRect(0, 0, canvas.width, canvas.height);
+    lightMap.fillRect(0, 0, mainCanvas.width, mainCanvas.height);
 
     /*====================*\
         #Create Shadows
@@ -115,8 +130,8 @@ function draw() {
 
     shadowContext.globalCompositeOperation = "xor";
     shadowContext.fillStyle = "#000";
-    shadowContext.fillRect(0, 0, canvas.width, canvas.height);
-    shadowContext.drawImage(lightMapCanvas, 0, 0, canvas.width, canvas.height);
+    shadowContext.fillRect(0, 0, mainCanvas.width, mainCanvas.height);
+    shadowContext.drawImage(lightMapCanvas, 0, 0, mainCanvas.width, mainCanvas.height);
 }
 
 
@@ -139,8 +154,6 @@ function update() {
     // FOR TESTING!!!!
     if (isKeyDown(" ")) {
         stopVal = (stopVal + 0.1) % 1;
-        console.log("space is Down!!", stopVal);// <.<
-
     }
 }
 
@@ -169,7 +182,59 @@ function isKeyDown(key) {
 
 
 
+/*====================*\
+    #TODO: Player
+\*====================*/
+
+let createPlayer = function () {
+
+};
+
+
+
+
 
 /*====================*\
-    #Muli-KeyPress
+    #Spawner
 \*====================*/
+
+// Create a new enemy every 3 seconds
+setInterval(function () {
+    enemies.push(createEnemy(mainCanvas));
+}, 3000);
+
+
+
+
+/*====================*\
+    #Enemies!
+\*====================*/
+
+let createEnemy = function (canvas) {
+    let health = 50;
+    let postion = {
+        x: Math.random() * canvas.width,
+        y: Math.random() * canvas.height,
+    };
+    let speed = 5;
+
+    let move = function (player) {
+        // how do they move?
+        // Walk straight for the player
+    };
+
+    let draw = function (ctx) {
+        ctx.fillStyle = "#FF0000";
+        ctx.beginPath();
+        ctx.arc(postion.x, postion.y, 10, 0, 2 * Math.PI);
+        ctx.stroke();
+        ctx.fill();
+    }
+
+    return {
+        health,
+        speed,
+        move,
+        draw
+    }
+};
