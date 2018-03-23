43a2572e0761e048eba1d71875d37fac458884bc
David Chung <davidc4747@yahoo.com>
2018-03-22 18:01:09 -0700

    Create Player class and looking at mouse

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
 
 
 /*====================*\
-    #Init Canvas
+    #Init Game
 \*====================*/
 
 const CANVAS_WIDTH = 800;
@@ -27,14 +27,11 @@ let lightMap = lightMapCanvas.getContext("2d");
 let shadowContext = shadowCanvas.getContext("2d");
 
 
-
-
 /* Enemy Vars */
 let enemies = [];
 
 /* Player Vars */
-let position = { x: mainCanvas.width / 2, y: mainCanvas.height / 2 };
-let speed = 7;
+let player = createPlayer(mainCanvas);
 let stopVal = 0;
 
 
@@ -49,7 +46,6 @@ setInterval(function () {
     update();
     draw();
 }, 100);
-// draw();
 
 
 
@@ -79,9 +75,7 @@ function draw() {
         #Draw Player
     \*====================*/
 
-    let playerImage = new Image();
-    playerImage.src = "./imgs/space-ship-svgrepo-com.svg"
-    mainContext.drawImage(playerImage, position.x, position.y, 50, 50);
+    player.draw(mainContext);
 
 
 
@@ -96,7 +90,7 @@ function draw() {
     lightMap.globalCompositeOperation = "lighter";
 
     // Create gradient
-    let playerLight = shadowContext.createRadialGradient(position.x, position.y, 50, position.x, position.y, 130);
+    let playerLight = shadowContext.createRadialGradient(player.position.x, player.position.y, 50, player.position.x, player.position.y, 130);
     playerLight.addColorStop(0, "white");
     playerLight.addColorStop(1, "transparent");
     lightMap.fillStyle = playerLight;
@@ -117,7 +111,7 @@ function draw() {
     lightMap.fillRect(0, 0, mainCanvas.width, mainCanvas.height);
 
     // Create Wave Gradient!!
-    let lightWave = shadowContext.createRadialGradient(position.x, position.y, mainCanvas.width * stopVal / 2, position.x, position.y, mainCanvas.width * stopVal);
+    let lightWave = shadowContext.createRadialGradient(player.position.x, player.position.y, mainCanvas.width * stopVal / 2, player.position.x, player.position.y, mainCanvas.width * stopVal);
     lightWave.addColorStop(0, "transparent");
     lightWave.addColorStop(stopVal, "white");
     lightWave.addColorStop(1, "transparent");
@@ -130,36 +124,42 @@ function draw() {
 
     shadowContext.globalCompositeOperation = "xor";
     shadowContext.fillStyle = "#000";
-    shadowContext.fillRect(0, 0, mainCanvas.width, mainCanvas.height);
-    shadowContext.drawImage(lightMapCanvas, 0, 0, mainCanvas.width, mainCanvas.height);
+    // shadowContext.fillRect(0, 0, mainCanvas.width, mainCanvas.height);
+    // shadowContext.drawImage(lightMapCanvas, 0, 0, mainCanvas.width, mainCanvas.height);
 }
 
 
 function update() {
-    // Check for keyboard input
-    if (isKeyDown("w")) {
-        position.y -= speed;
-    }
-    if (isKeyDown("a")) {
-        position.x -= speed;
-    }
-    if (isKeyDown("s")) {
-        position.y += speed;
-    }
-    if (isKeyDown("d")) {
-        position.x += speed;
-    }
+
+    /*====================*\
+        #Update Enemies
+    \*====================*/
+
+    enemies.forEach(function (enemy) {
+        enemy.update(player);
+    });
 
 
-    // FOR TESTING!!!!
-    if (isKeyDown(" ")) {
-        stopVal = (stopVal + 0.1) % 1;
-    }
+
+
+    /*====================*\
+        #Update Player
+    \*====================*/
+    player.update();
 }
 
 
 
 
+/*====================*\
+    #Mouse position
+\*====================*/
+let mousePosition = { x: 0, y: 0 };
+
+document.addEventListener("mousemove", function (e) {
+    mousePosition.x = e.clientX;
+    mousePosition.y = e.clientY;
+});
 
 /*====================*\
     #Muli-KeyPress
@@ -167,7 +167,6 @@ function update() {
 
 // TODO: Move key detection into it's own module
 // Create a Map of key states to allow detection of multiple KeyPresses
-
 let keyMap = {};
 let keyCheck = function (e) {
     keyMap[e.key.toLowerCase()] = e.type === "keydown";
@@ -183,11 +182,64 @@ function isKeyDown(key) {
 
 
 /*====================*\
-    #TODO: Player
+    #Player
 \*====================*/
 
-let createPlayer = function () {
+function createPlayer(canvas) {
+    let playerImage = new Image();
+    playerImage.src = "./imgs/space-ship-svgrepo-com.svg";
 
+    let health = 100;
+    let rotation = 0;
+    let position = { x: canvas.width / 2, y: canvas.height / 2 };
+    let speed = 7;
+
+    let update = function () {
+        // Check for keyboard input
+        if (isKeyDown("w")) {
+            position.y -= speed;
+        }
+        if (isKeyDown("a")) {
+            position.x -= speed;
+        }
+        if (isKeyDown("s")) {
+            position.y += speed;
+        }
+        if (isKeyDown("d")) {
+            position.x += speed;
+        }
+
+
+        // FOR TESTING!!!!
+        if (isKeyDown(" ")) {
+            stopVal = (stopVal + 0.1) % 1;
+        }
+
+    };
+
+    let draw = function (ctx) {
+        // Rotate toward the mouse
+        let lookVector = {
+            x: mousePosition.x - position.x,
+            y: mousePosition.y - position.y
+        };
+
+
+        ctx.save();
+        ctx.translate(position.x , position.y);
+        ctx.rotate(Math.atan2(lookVector.x, -lookVector.y));
+        ctx.drawImage(playerImage, -25, -25, 50, 50);
+        ctx.restore();
+
+    };
+
+
+    return {
+        health,
+        position,
+        update,
+        draw
+    };
 };
 
 
@@ -203,14 +255,11 @@ setInterval(function () {
     enemies.push(createEnemy(mainCanvas));
 }, 3000);
 
-
-
-
 /*====================*\
     #Enemies!
 \*====================*/
 
-let createEnemy = function (canvas) {
+function createEnemy(canvas) {
     let health = 50;
     let postion = {
         x: Math.random() * canvas.width,
@@ -218,12 +267,15 @@ let createEnemy = function (canvas) {
     };
     let speed = 5;
 
-    let move = function (player) {
+    let update = function (player) {
         // how do they move?
         // Walk straight for the player
     };
 
     let draw = function (ctx) {
+        if (health <= 0)
+            return;// TODO: remove enemy from memory
+
         ctx.fillStyle = "#FF0000";
         ctx.beginPath();
         ctx.arc(postion.x, postion.y, 10, 0, 2 * Math.PI);
@@ -234,7 +286,7 @@ let createEnemy = function (canvas) {
     return {
         health,
         speed,
-        move,
+        update,
         draw
     }
 };
